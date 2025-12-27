7f680c57d6c4655d3635ee73a1a4c52663ac81e9
careatomkarenterprises-ops <careatomkarenterprises@gmail.com>
2025-12-27 16:43:35 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/omkar_enterprises_test/',   // 👈 must match repo name exactly
-})
