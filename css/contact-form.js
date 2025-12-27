dce96f61cf52f3cea147fbb69b8b40f21fdcaa8d
careatomkarenterprises-ops <careatomkarenterprises@gmail.com>
2025-12-28 03:47:21 +0530

    Delete css/contact-form.js

--- css/contact-form.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-  const btn = e.target.querySelector("button");
-  btn.innerText = "Sending...";
-  btn.disabled = true;
-});
